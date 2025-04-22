b0a59a07c5e0a3f4fc2d6c2410853a73a7b32507
BerleLBowen <berlebowen@yahoo.com>
2025-04-22 14:23:59 -0600

    Puppy Pals

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,33 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  console.log('hello worlds')
   console.log(puppies)
 
+  const [featPupId, setFeatPupId] = useState(null)
+
+
+  function handleClick(id) {
+    setFeatPupId(id)
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
     <>
       <div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        
       { 
    puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>
+     return <p onClick={() => handleClick(puppy.id)} key={puppy.id}>{puppy.name}</p>
    })
 }
       </div>
